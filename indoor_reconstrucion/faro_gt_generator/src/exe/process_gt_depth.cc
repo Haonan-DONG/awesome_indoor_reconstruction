af1691d2639897b468995b1c6687178b68a598f0
haonan-dong <august_dong@163.com>
2022-10-20 21:40:03 +0800

    change the gt float to png

--- indoor_reconstrucion/faro_gt_generator/src/exe/process_gt_depth.cc
+++ indoor_reconstrucion/faro_gt_generator/src/exe/process_gt_depth.cc
@@ -20,22 +20,23 @@ int main(int argc, char **argv)
     std::string ground_truth_depth_path;
     pcl::console::parse_argument(argc, argv, "--ground_truth_depth_path", ground_truth_depth_path);
 
-    cv::Mat gt_depth_map(1440, 1920, CV_32FC1);
-    cv::Mat gt_depth_map_int(1440, 1920, CV_16U);
+    cv::Mat gt_depth_map(4032, 6048, CV_32FC1);
+    cv::Mat gt_depth_map_int(4032, 6048, CV_16UC1);
 
     FILE *ground_truth_depth_file = fopen(ground_truth_depth_path.c_str(), "rb");
     fread(gt_depth_map.data, sizeof(float), gt_depth_map.rows * gt_depth_map.cols, ground_truth_depth_file);
     fclose(ground_truth_depth_file);
 
-    int scale = 1000;
+    float scale = 1000.0;
 
     for (int i = 0; i < gt_depth_map.rows; i++)
     {
         for (int j = 0; j < gt_depth_map.cols; j++)
         {
             float *depth_value = gt_depth_map.ptr<float>(i, j);
-            uint16_t depth_int = *depth_value * scale;
-            gt_depth_map_int.ptr<uint16_t>(i, j)[0] = depth_int;
+            u_short depth_int = (*depth_value) * scale;
+
+            gt_depth_map_int.ptr<u_short>(i, j)[0] = depth_int;
         }
     }
 
@@ -43,10 +44,10 @@ int main(int argc, char **argv)
     cv::normalize(gt_depth_map_int, normal_depth, 0, 255, cv::NORM_MINMAX);
     cv::Mat result;
     normal_depth.convertTo(result, CV_8UC1);
-    cv::imwrite("depth.png", result);
+    cv::imwrite("depth_normal.png", result);
 
     std::cout << "Saving GT depth."
               << std::endl;
-    // cv::imwrite("depth.png", gt_depth_map_int);
+    cv::imwrite("depth.png", gt_depth_map_int);
     return 0;
 }
