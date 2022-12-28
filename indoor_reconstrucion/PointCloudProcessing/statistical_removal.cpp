9b9c98cebe05002abbf83576d129ef5c2155592a
Haonan-DONG <august_dong@163.com>
2022-12-28 16:39:01 +0800

    add color info

--- indoor_reconstrucion/PointCloudProcessing/statistical_removal.cpp
+++ indoor_reconstrucion/PointCloudProcessing/statistical_removal.cpp
@@ -12,11 +12,11 @@ int main(int argc, char **argv)
     std::string point_cloud_path_out;
     pcl::console::parse_argument(argc, argv, "--output", point_cloud_path_out);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
 
     // Fill in the cloud data
-    if (pcl::io::loadPLYFile<pcl::PointXYZ>(point_cloud_path, *cloud) == -1) //* load the file
+    if (pcl::io::loadPLYFile<pcl::PointXYZRGB>(point_cloud_path, *cloud) == -1) //* load the file
     {
         PCL_ERROR("Couldn't read file test_pcd.pcd \n");
         system("PAUSE");
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
     std::cerr << *cloud << std::endl;
 
     // Create the filtering object
-    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
     sor.setInputCloud(cloud);
     sor.setMeanK(50);
     sor.setStddevMulThresh(1.0);
